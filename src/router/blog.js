94e3f9758b4077c4ea6c3f1b65803f4ffddb5ade
yangjiaxin <yangjiaxin@dhgate.com>
2025-09-10 16:26:39 +0800

    feat: 管理限制只能编辑当前登录用户的博客

--- src/router/blog.js
+++ src/router/blog.js
@@ -21,8 +21,15 @@ const handleBlogRouter = (req, res) => {
 
   // 获取博客列表
   if (method === "GET" && path === "/api/blog/list") {
-    const author = req.query.author || "";
+    let author = req.query.author || "";
     const keyword = req.query.keyword || "";
+    if (req.query.isadmin) {
+      const loginCheckResult = loginCheck(req);
+      if (loginCheckResult) {
+        return loginCheckResult;
+      }
+      author = req.session.username;
+    }
     const result = getList(author, keyword);
     return result.then((listData) => {
       return new SuccessModel(listData);
